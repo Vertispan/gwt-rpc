122fc32e03d83b5f15d57b38d2b21571fb84f82d
Zach Visagie <zachv@ipay.co.za>
2020-01-14 10:06:08 +0200

    Make it possible to create headers based on a function

--- endpoint-client-gwt/src/main/java/org/gwtproject/rpc/gwt/client/HttpHeader.java
+++ endpoint-client-gwt/src/main/java/org/gwtproject/rpc/gwt/client/HttpHeader.java
@@ -1,52 +1,59 @@
 package org.gwtproject.rpc.gwt.client;
 
-public class HttpHeader {
+/**
+ * Represents an HttpHeader. Hashcode and equals are based on the name of the header.
+ * 
+ * A subclass can override getValue making it possible to resolve the value based on a function this deferring
+ * value resolution to the moment it is required.
+ *
+ */
+public abstract class HttpHeader {
 	private String name;
-	private String value;
 	
-	public HttpHeader(String name, String value) {
+	public HttpHeader(String name) {
 		super();
+		if(name == null) {
+		    throw new IllegalArgumentException("Http header name cannot be null");
+		}
 		this.name = name;
-		this.value = value;
 	}
 	
 	public String getName() {
 		return name;
 	}
 	
-	public String getValue() {
-		return value;
-	}
+	public abstract String getValue();
 
+	
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((value == null) ? 0 : value.hashCode());
-		return result;
-	}
+    public String toString() {
+        return "HttpHeader [name=" + name + "]";
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		HttpHeader other = (HttpHeader) obj;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (value == null) {
-			if (other.value != null)
-				return false;
-		} else if (!value.equals(other.value))
-			return false;
-		return true;
-	}
+    /**
+	 * Based on header name
+	 */
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    /**
+     * Two headers are consider equal if their names are equal
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        HttpHeader other = (HttpHeader) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
 	
 }
